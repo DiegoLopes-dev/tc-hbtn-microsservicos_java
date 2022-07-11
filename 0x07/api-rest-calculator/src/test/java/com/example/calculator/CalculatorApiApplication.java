53cc0d50f36db39067e809c0878f89c663c33b07
Diego Lopes Mercado <diegolm@ciandt.com>
2022-07-11 12:12:22 -0300

    Criando um aplicação API REST Calculator + JUNIT

--- 0x07/api-rest-calculator/src/test/java/com/example/calculator/CalculatorApiApplication.java
+++ 0x07/api-rest-calculator/src/test/java/com/example/calculator/CalculatorApiApplication.java
@@ -1,10 +1,10 @@
-package com.example.calculator;
+ackage com.example.calculator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CalculatorApiApplicationTests {
+class CalculatorApplicationTests {
 
     @Test
     void contextLoads() {
